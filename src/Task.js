24a7c0509794b6f11cf399f08c8993d8f7c8c608
MohAkbar19 <akbarbarxx@gmail.com>
2024-10-27 07:14:04 +0700

    Exercise Completed

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "" // TODO: replace this
+const studentName = "Mohamad Akbar Hernanda" // TODO: replace this
+const studentId = "FS9464154" // TODO: replace this
+
+export { url, studentName, studentId }
